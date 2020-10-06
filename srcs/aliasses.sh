a0babbf167bd5befd4be1dfdb1bda8392f70276c
Arthur Gossuin <agossuin@mi-r2-p4.s19.be>
2020-10-06 16:27:50 +0200

    new aliasses for macos = code + atom

--- srcs/aliasses.sh
+++ srcs/aliasses.sh
@@ -2,3 +2,7 @@ alias github='cd ~/Desktop/Artygo8-github'
 alias fclean='make fclean'
 alias run='make run'
 alias python=python3
+if [ `uname` == "Darwin" ]; then
+	alias code='open -a "Visual Studio Code 2"'
+	alias atom='open -a "Atom"'
+fi
